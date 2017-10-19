0e707c3b0767b3b134b09db5e1da79387e596b2b
Dino Esposito <dinoesp@hotmail.com>
2017-10-19 22:03:21 +0200

    1

---
+++ Src/Ch01/HelloWorldConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿//////////////////////////////////////////////////////////////////
+//
+//   PROGRAMMING ASP.NET CORE
+//   Dino Esposito
+//   
+//   Ch01 - Why Another (ASP).NET
+//   HelloWorldConsole
+//
+
+using System;
+
+namespace Ch01.HelloWorldConsole
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}

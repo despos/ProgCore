71760f7cfd9bc6aba23369e8a12867f0a8353343
Dino Esposito <dinoesp@hotmail.com>
2019-01-08 13:49:34 +0100

    1.0

---
+++ Src/Extras/Blazor Demos/0.7.0/Forms/Forms.App/Common/ModalContext.cs
@@ -0,0 +1,17 @@
+﻿//////////////////////////////////////////////////////////////////
+//
+//   PROGRAMMING ASP.NET CORE
+//   Dino Esposito
+//   
+//   Ch06 - The Razor Syntax 
+//   BootstrapTagHelpers
+//
+
+namespace Forms.App.Common
+{
+    public class ModalContext
+    {
+        public string Id { get; set; }
+        public bool AutoClose { get; set; }
+    }
+}
